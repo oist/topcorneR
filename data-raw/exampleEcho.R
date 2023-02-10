7f9716fcfda75e4f6f9341b16fc22a327f67deae
Charles Plessy <charles.plessy@oist.jp>
2023-02-10 09:13:33 +0900

    Add logs to the example Echo object.

--- data-raw/exampleEcho.R
+++ data-raw/exampleEcho.R
@@ -1,6 +1,11 @@
-exampleEcho <- Echo( source      = examplePlate
-                   , destination = Plate("96", deadVolume = 1e4, maxVolume = 1e5)
-                   , transducer  = exampleTransducer
-                   , model       = "525")
+exampleEcho <- planTransfers(
+  source = examplePlate,
+  destination = Plate("96", deadVolume = 1e4, maxVolume = 1e5),
+  model="525",
+  plan = Plate("384") |>
+    setWell(Well("A01"), "dNTP",   50) |>
+    setWell(Well("A02"), "dNTP",  100) |>
+    setWell(Well("A01"), "buffer", 50)
+)
 
 usethis::use_data(exampleEcho, overwrite = TRUE)
