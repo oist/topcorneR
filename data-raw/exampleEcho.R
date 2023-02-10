1b38b0f1c687986d9f219a25ee26415463835d6d
Charles Plessy <charles.plessy@oist.jp>
2023-02-10 14:14:15 +0900

    Correct plan plate format (must be same as in destination)

--- data-raw/exampleEcho.R
+++ data-raw/exampleEcho.R
@@ -2,7 +2,7 @@ exampleEcho <- planTransfers(
   source = examplePlate,
   destination = Plate("96", deadVolume = 1e4, maxVolume = 1e5),
   model="525",
-  plan = Plate("384") |>
+  plan = Plate("96") |>
     setWell(Well("A01"), "dNTP",   50) |>
     setWell(Well("A02"), "dNTP",  100) |>
     setWell(Well("A01"), "buffer", 50)
