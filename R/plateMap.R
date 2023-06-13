4140de1de2ef1989e04399f15376676ff7821a2d
Charles Plessy <charles.plessy@oist.jp>
2023-06-14 08:31:28 +0900

    Functions to plot contents of plates.
    Taken from old vignettes of the nanoCAGE and eDNA projects.

---
+++ R/plateMap.R
@@ -0,0 +1,36 @@
+#' Plot the contents of a plate
+#'
+#' @param plate a [`Plate`] object.
+#' @param x One reagent of a plate.
+#' @param title a title for the plot.
+#'
+#' @return A [`ggplot2::ggplot`] object.
+#'
+#' @author Charles Plessy
+#'
+#' @export
+
+plateMap <- function(plate, x, title=x) {
+  if(isFALSE(requireNamespace('ggplot2'))) stop("Please install the ggplot2 package to use this function.")
+  platetools::raw_map(plate[[x]], well=rownames(plate), plate=plate@metadata$type) +
+    ggplot2::ggtitle(title) +
+    viridis::scale_fill_viridis(trans = "log")
+}
+
+#' @rdname plateMap
+#' @export
+
+plateMap_all <- function(plate) {
+  if(isFALSE(requireNamespace('ggplot2'))) stop("Please install the ggplot2 package to use this function.")
+  x <- lapply(colnames(plate), function(x) ifelse(is.na(plate[,x]), "", x))
+  names(x) <- colnames(plate)
+  x <- as.data.frame(x)
+  x <- apply( x, 1
+              , function(x)
+                if(all(x=="")) {
+                  NA
+                } else {
+                  paste(unique(x[x!=""]), collapse=",")
+                })
+  platetools::raw_map(x, well=rownames(plate), plate="384")
+}
