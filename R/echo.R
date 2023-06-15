607d0a6399b17d22faa0160ca26baca7753fb13e
Charles Plessy <charles.plessy@oist.jp>
2023-06-15 15:25:47 +0900

    Give extra information in error message.

--- R/echo.R
+++ R/echo.R
@@ -428,7 +428,7 @@ planTransfers <- function(source, destination, plan, model = "525") {
 
 setMethod("seekReagent", c("Echo", "character", "Well"), function(object, reagent, start) {
   newPos <- seekReagent(object@source, reagent, start)
-  if (suppressWarnings(is.na(newPos))) stop(paste0("Reagent ", reagent," not found !"))
+  if (suppressWarnings(is.na(newPos))) stop(paste0("Reagent ", reagent," not found after emptying well ", start@well,"!"))
   object |> moveTransducer(source = newPos)
 })
 
